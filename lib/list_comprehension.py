a2a80372e45d7f9700f3302c0bc32dc1275e650e
Abdulaziz Abdi <amigf76@gmail.com>
2024-05-27 18:00:01 +0300

    List comprehension labs

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [number for number in num_list if number % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [(f"{string}!") for string in sentence_list]
+    return exclamation_list
